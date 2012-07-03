c8e6489faf9e196e36898bd014fd77c093dbdfe1
estebanSannin <stefanoviola85@gmail.com>
2012-07-04 00:48:16 +0200

    Aggiunte nuove funzionalita'.
    - Aggiunta gestione del system.xml:
       ora e' possibile settare il titolo
       del sito, il sotto titolo e il tema
       direttamente dall'xml: "system.xml"
    
    - Sistemati messaggi di errore..

--- system/xblog.js
+++ system/xblog.js
@@ -10,24 +10,47 @@
 
 $(document).ready(function(){
 
-  if($.browser.msie==true){
-    alert("IE Browser in not supported!");
-    $('#content').html("This site is not supported by Internet Explorer.<br>Install a browser actual: Firefox, Opera or Chrome.");
-    return false;
-  }
-  $("<h3>Blog Post:</h3>").appendTo('#postBlog');
-  var i = 0;
-  $.ajax({ type: "GET", url: "resources/post.xml", dataType: "xml",
-     
-    success: function(xml) {
-       
-      $(xml).find('post').each(function() {
-        var linkID = 'link'+i;//$(this).find('linkID').text();
-	var postID = 'post'+i;//$(this).find('postID').text();
-	var data = $(this).find('date').text();
-        var titolo = $(this).find('title').text();
-        var content = $(this).find('content').text();
-        var link_markup = '<li>'+data+' <a href="#" id="'+linkID+'">'+titolo+'</a><br><div class="comment" id="'+postID+'">'+content+'<p><a href="#" id="close'+i+'">CLOSE</a></p></div></li>';
+if($.browser.msie==true){
+	alert("IE Browser in not supported!");
+	$('#content').html("This site is not supported by Internet Explorer.<br>Install a browser actual: Firefox, Opera or Chrome.");
+	return false;
+}
+	
+	// System
+	
+	$.ajax({ type: "GET", url: "resources/system.xml", dataType: "xml",
+
+	success: function(xml) {
+
+	$(xml).find('xblog').each(function() {
+		var siteName = $(this).find('siteName').text();
+		var theme = $(this).find('theme').text();
+		var subTitle = $(this).find('subTitle').text();
+		$("#theme").attr("href", "themes/" + theme + ".css");
+		$(siteName).appendTo('#header2');
+		$(subTitle).appendTo('#subtitle');
+	});
+	},
+	error: function() { alert("XBlog: Error generating system attributes!"); }
+	});
+
+
+
+	// Post Blog
+
+	$("<h3>Blog Post:</h3>").appendTo('#postBlog');
+	var i = 0;
+	$.ajax({ type: "GET", url: "resources/post.xml", dataType: "xml",
+
+	success: function(xml) {
+
+	$(xml).find('post').each(function() {
+		var linkID = 'link'+i;
+		var postID = 'post'+i;
+		var data = $(this).find('date').text();
+		var title = $(this).find('title').text();
+		var content = $(this).find('content').text();
+		var link_markup = '<li>'+data+' <a href="#" id="'+linkID+'">'+title+'</a><br><div class="comment" id="'+postID+'">'+content+'<p><a href="#" id="close'+i+'">CLOSE</a></p></div></li>';
 
 	$(link_markup).appendTo('#postBlog');
 	$('#'+postID).hide();
@@ -39,40 +62,28 @@ $(document).ready(function(){
 	  $('#'+postID).slideUp('fast');
 	});
 	i++;
-      });    
-    },
-    error: function(request, error, tipo_errore) { alert(error+': '+ tipo_errore); }
-  });
+	});
+	},
+	error: function() { alert("XBlog: Error generating post blog!"); }
+	});
 
-  $.ajax({ type: "GET", url: "resources/menu.xml", dataType: "xml",
-     
-    success: function(xml) {
-       
-      $(xml).find('menu').each(function() {
-        var menuID = $(this).find('id').text();
-        var titolo = $(this).find('title').text();
-        var url = $(this).find('url').text();
-        var link_markup = '<td class="headitem"><a href="'+url+'" class="menu" id="'+menuID+'">'+titolo+'</a></td>';
+// Menu management
 
-        $(link_markup).appendTo('#menuBar');
-      });    
-    },
-    error: function(request, error, tipo_errore) { alert(error+': '+ tipo_errore); }
-  });
+	$.ajax({ type: "GET", url: "resources/menu.xml", dataType: "xml",
 
+	success: function(xml) {
 
-/*
-$(".menu").click(function(){
-	a = $(this).attr("id");
-	page = $(this).attr("href");
-	alert(a);
-	alert(page);
+	$(xml).find('menu').each(function() {
+		var menuID = $(this).find('id').text();
+		var titolo = $(this).find('title').text();
+		var url = $(this).find('url').text();
+		var link_markup = '<td class="headitem"><a href="'+url+'" class="menu" id="'+menuID+'">'+titolo+'</a></td>';
 
-
-});
-  */
-//$('#content').text(content);
-  
+		$(link_markup).appendTo('#menuBar');
+	});    
+	},
+	error: function() { alert("XBlox: Error generating menu"); }
+	});
 
 });
 

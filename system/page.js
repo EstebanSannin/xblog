547e3d444a3e81f9673e7835807f4e05f2cab003
Stefano Viola <stefanoviola85@gmail.com>
2017-07-12 03:48:55 +0200

    First draft of xBl0g 2.0

--- system/page.js
+++ system/page.js
@@ -8,18 +8,35 @@
  */
 
 $(document).ready(function(){
-	$.ajax({ type: "GET", url: "resources/page.xml", dataType: "xml",
+
+	$.ajax({type: "GET", url: "resources/page.xml", dataType: "xml",
 	
 	success: function(xml) {
 		$(xml).find('page').each(function() {
 		var tag = $(this).find('tag').text();
+		var markup = $(this).find('markup').text();
 		var content = $(this).find('content').text();
 
-		$('a#'+tag).live("click", function(){
-		 $('#content').hide();
-		 $('#content').html(content);
-		 $('#content').fadeIn(1500);
-		});
+		console.log("TAG FROM PAGE: "+tag);
+		console.log("Markup: " + markup);
+		$('a#'+tag).click(function(){
+			console.log("CLICKED: "+tag+" Markup: "+markup);
+				$('#wrapper').hide();
+				$('#wrapper').html("<h3>"+tag+"</h3>");
+				if(markup == "MARKDOWN") {
+					console.log("Converting from MARKDOWN to HTML");
+					var converter = new showdown.Converter(),
+						text      = content,
+						html      = converter.makeHtml(text);
+				} else {
+					html = content;
+				}
+				$('#wrapper').append(html);
+				$('pre code').each(function(i, block) {
+					hljs.highlightBlock(block);
+				});
+				$('#wrapper').fadeIn(1500);
+			});
 
 		});
 	},
--- system/xblog.js
+++ system/xblog.js
@@ -1,42 +1,40 @@
-/*
+/******************************************************************************
  *
  *    NAME: XBLOG
- * VERSION: 0.1.0
+ * VERSION: 0.2.0
  *  AUTHOR: Stefano Viola (estebanSannin)
- * 
  *
- *
- */
+ ******************************************************************************/
 
 $(document).ready(function(){
 
-if($.browser.msie==true){
-	alert("IE Browser in not supported!");
-	$('#content').html("This site is not supported by Internet Explorer.<br>Install a browser actual: Firefox, Opera or Chrome.");
-	return false;
-}
-	
 	// System
 	
 	$.ajax({ type: "GET", url: "resources/system.xml", dataType: "xml",
 
 	success: function(xml) {
-
-	$(xml).find('xblog').each(function() {
-		var siteName = $(this).find('siteName').text();
-		var theme = $(this).find('theme').text();
-		var subTitle = $(this).find('subTitle').text();
-		$("#theme").attr("href", "themes/" + theme + ".css");
-		$(siteName).appendTo('#header2');
-		$(subTitle).appendTo('#subtitle');
-	});
+		$(xml).find('xblog').each(function() {
+			var siteName = $(this).find('siteName').text();
+			var theme = $(this).find('theme').text();
+			var subTitle = $(this).find('subTitle').text();
+			var homepage_content = $(this).find('homepage').text();
+			$("#theme").attr("href", "themes/" + theme + ".css");
+			$(siteName).appendTo('#header');
+			showdown.parseImgDimension=true;
+			var converter = new showdown.Converter(),
+				text      = homepage_content,
+				html      = converter.makeHtml(text);
+			console.log("Home page content: "+homepage_content);
+			$("#textblog").html(html)
+			$(subTitle).appendTo('#subtitle');
+		});
 	},
 	error: function() { alert("XBlog: Error generating system attributes!"); }
 	});
 
+	// Home page content
 
-
-	// Post Blog
+			// Post Blog
 
 	$("<h3>Blog Post:</h3>").appendTo('#postBlog');
 	var i = 0;
@@ -45,21 +43,44 @@ if($.browser.msie==true){
 	success: function(xml) {
 
 	$(xml).find('post').each(function() {
+		console.log("EXECUTE post");
 		var linkID = 'link'+i;
 		var postID = 'post'+i;
 		var data = $(this).find('date').text();
 		var title = $(this).find('title').text();
-		var content = $(this).find('content').text();
-		var link_markup = '<li>'+data+' <a href="#" id="'+linkID+'">'+title+'</a><br><div class="comment" id="'+postID+'">'+content+'<p><a href="#" id="close'+i+'">CLOSE</a></p></div></li>';
-
-	$(link_markup).appendTo('#postBlog');
-	$('#'+postID).hide();
+		var author = $(this).find('author').text();
+		var content_post = $(this).find('content').text();
+		var markup = $(this).find('markup').text();
+		if(markup == "MARKDOWN") {
+			console.log("[xblog.js]: Converting from MARKDOWN to HTML");
+			var converter = new showdown.Converter(),
+				text      = content_post,
+				html      = converter.makeHtml(text);
+		} else {
+			console.log("[xblog.js]: Markup: HTML");
+			html = content_post;
+		}
+		var link_markup = '<li>'+data+
+			' <a href="#'+linkID+'" id="'+linkID+'">'+title+
+			'</a></li>';
+		var altro='<br><div class="comment2" id="'+postID+
+			'"><center><h2>'+title+'</h2>writed: '+data+' - Author: '+author+'</center><br>'+html+
+			'</div>';
+		$('#postBlog').append(link_markup);
+		$('#'+postID).hide();
 	
-	$('a#'+linkID).click(function(){
-	  $('#'+postID).slideToggle('fast');
-	});
+		$('a#'+linkID).click(function(){
+			console.log("CLICKED: "+linkID);
+			$('#textblog').html(altro);
+			$('pre code').each(function(i, block) {
+				console.log("highlight");
+				hljs.highlightBlock(block);
+			});
+			$('#'+postID).slideDown('fast');
+	})
 	$('a#close'+i).click(function(){
 	  $('#'+postID).slideUp('fast');
+		$('#disq'+linkID).empty();
 	});
 	i++;
 	});
